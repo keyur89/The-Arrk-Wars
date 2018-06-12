e2a629454d797523f19e8fda619a1eb24af9a11c
Keyur Tailor <keyur.tailor89@gmail.com>
2018-06-12 22:36:02 +0530

    Added test class.

--- app/src/androidTest/java/project/aark/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/project/aark/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("project.mvpdemo", appContext.getPackageName());
+        assertEquals("project.aark", appContext.getPackageName());
     }
 }
---
+++ app/src/androidTest/java/project/aark/starWars/view/SWPeopleActivityTest.java
@@ -0,0 +1,40 @@
+package project.aark.starWars.view;
+
+import android.support.test.filters.MediumTest;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by Keyur Tailor on 12-Jun-2018.
+ */
+@MediumTest
+@RunWith(AndroidJUnit4.class)
+public class SWPeopleActivityTest {
+
+    @Test
+    public void refreshLoading() throws Exception {
+    }
+
+    @Test
+    public void checkIfNetworkConnected() throws Exception {
+    }
+
+    @Test
+    public void showLoading() throws Exception {
+    }
+
+    @Test
+    public void setAdapterData() throws Exception {
+    }
+
+    @Test
+    public void SWDetailsSuccessEvent() throws Exception {
+    }
+
+    @Test
+    public void SWDetailFailureEvent() throws Exception {
+    }
+
+}
